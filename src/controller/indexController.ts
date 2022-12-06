7058aac418d330f74f6f3a34858ebf1c7eb6a9e2
“julianmendonca” <julianmnendon@gmail.com>
2022-12-06 19:46:42 -0300

    imitial commit

--- src/controller/indexController.ts
+++ src/controller/indexController.ts
@@ -1,19 +1,20 @@
-import { FastifyInstance, FastifyRequest, FastifyReply } from "fastify";
-import { promises } from "fs";
-import { resolve } from "path";
-
-const { readFile } = promises;
+import { FastifyInstance, FastifyRequest } from "fastify";
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
+  apiKey:
+    process.env.OPENAI_API_KEY ||
+    "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",
+});
+const openai = new OpenAIApi(configuration);
 
 export default async function indexController(fastify: FastifyInstance) {
   // GET /
-  fastify.get("/", async function (
-    _request: FastifyRequest,
-    reply: FastifyReply
-  ) {
-    const indexHtmlPath = resolve(__dirname, "../../static/index.html");
-    const indexHtmlContent = await readFile(indexHtmlPath);
-    reply
-      .header("Content-Type", "text/html; charset=utf-8")
-      .send(indexHtmlContent);
+  fastify.get("/", async function (_request: FastifyRequest) {
+    const response = await openai.createImage({
+      prompt: "a white siamese cat",
+      n: 1,
+      size: "256x256",
+    });
+    return response.data.data[0].url;
   });
 }
